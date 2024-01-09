b11db91ab2f213bc4585de4cba5503537cc8e092
byeonsejun <qustpwns93@gmail.com>
2024-01-09 16:11:30 +0900

    add input name

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navigation = () => {
   const navigate = useNavigate();
@@ -12,21 +12,23 @@ const Navigation = () => {
   const getSearchName = (event) => {
     event.preventDefault();
     navigate(`/movies/?s=${event.target[0].value}&p=1&f=1`);
-    event.target[0].value = "";
-  }
+    event.target[0].value = '';
+  };
   const goToSearch = () => {
     let searchInput = document.querySelector('.me-2.form-control').value;
     navigate(`/movies/?s=${searchInput}&p=1&f=1`);
-    document.querySelector('.me-2.form-control').value = "";
-  }
+    document.querySelector('.me-2.form-control').value = '';
+  };
 
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container fluid>
-          <Navbar.Brand href="" className='mainLogo'>
+          <Navbar.Brand href="" className="mainLogo">
             <img
-              onClick={()=>{ navigate("/") }}
+              onClick={() => {
+                navigate('/');
+              }}
               width={100}
               src="https://sejun-netflix.netlify.app/logo.png"
               alt="logoimg"
@@ -34,22 +36,25 @@ const Navigation = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Link className="nav-item" to="/">Home</Link>
-              <Link className="nav-item" to="/movies">Movies</Link>
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+              <Link className="nav-item" to="/">
+                Home
+              </Link>
+              <Link className="nav-item" to="/movies">
+                Movies
+              </Link>
             </Nav>
-            <Form className="d-flex" onSubmit={(event)=>getSearchName(event)}>
+            <Form className="d-flex" onSubmit={(event) => getSearchName(event)}>
               <Form.Control
+                name="search Movie"
                 type="search"
                 placeholder="Search"
                 className="me-2 search_input"
                 aria-label="Search"
               />
-              <Button variant="outline-danger" className='search_btt' onClick={goToSearch} >Search</Button>
+              <Button variant="outline-danger" className="search_btt" onClick={goToSearch}>
+                Search
+              </Button>
             </Form>
           </Navbar.Collapse>
         </Container>
